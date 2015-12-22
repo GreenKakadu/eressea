5f8be70bc584ab34f6a685716e4941ff766ca68a
Enno Rehling <enno.rehling@gmail.com>
2015-12-22 14:38:56 +0100

    it's beginning to look a lot like christmas

--- scripts/eressea/xmas.lua
+++ scripts/eressea/xmas.lua
@@ -1,7 +1,7 @@
 local gifts = {
     e2 = {
-        -- { year = 2015, turn = 960, item = 'snowglobe' },
-        { year = 2009, turn = 624, key = 'xm09', item = 'xmastree' },
+        { year = 2015, turn = 959, item = 'snowglobe', msg='santa_f' },
+        { year = 2009, turn = 624, item = 'xmastree' },
         { year = 2006, turn = 468, key = 'xm06', item = 'snowman' },
         { year = 2005, turn = 416, key = 'xm05', item = 'stardust' },
         { year = 2004, turn = 364, key = 'xm04', item = 'speedsail' }
@@ -14,10 +14,17 @@ local gifts = {
 
 local function give_gifts(gift)
     eressea.log.info("Es weihnachtet sehr (" .. gift.year .. ")")
+    local msg = nil
+    if gift.msg then
+        msg = message.create(gift.msg)
+        msg:set_resource("item", gift.item)
+    end
     if gift.item then
         for f in factions() do
             f:add_item(gift.item, 1)
-            f:add_notice("santa" .. gift.year)
+            if msg then
+                msg:send_faction(f)
+            end
         end
     end
 end
